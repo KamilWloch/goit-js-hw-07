d343073e865067f9ea0cd1f57c368dc58718c978
KamilWloch <109965679+KamilWloch@users.noreply.github.com>
2023-07-04 21:06:58 +0200

    Update 02-lightbox.js
    Typo fixes

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,7 +5,7 @@ const gallery = document.querySelector(".gallery");
 
 const markup = galleryItems
     .map(
-        (item) => `<a class="gallery__item" href=${item.original}><img class="gallery__image" src=${item.preview} alt=&{item.description}/></a>`
+        (item) => `<a class="gallery__item" href=${item.original}><img class="gallery__image" src=${item.preview} alt=${item.description}/></a>`
     )
     .join("");
 gallery.innerHTML = markup;
