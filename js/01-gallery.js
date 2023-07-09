eb0b84be4e8cc3df157d5972a65c3638caaae4b3
KamilWloch <109965679+KamilWloch@users.noreply.github.com>
2023-07-09 22:30:06 +0200

    Update 01-gallery.js
    Fixes

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,40 +1,68 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-const gallery = document.querySelector(".gallery");
-const markup = galleryItems
-    .map(
-        (item) => `<div class="gallery__item"><a class="gallery__link" href=${item.original}><img class="gallery__image" src=${item.preview} data-source=${item.original} alt=$item.description}/></a></div>`
-    )
+const gallery = document.querySelector("ul.gallery");
+
+const galleryImages = galleryItems
+    .map(image => 
+        `<div class = "gallery__item">
+            <a class = "gallery__link" href = "${image.original}">
+                <img class = "gallery__image" data-source = "${image.original}" src = "${image.preview}" alt = "${image.description}" />
+            </a>
+        </div>`)
     .join("");
-gallery.innerHTML = markup;
 
-let instance;
+gallery.insertAdjacentHTML("beforeend", galleryImages);
+console.log(gallery);
 
-gallery.addEventListener("click", (event) => {
-    event.preventDefault();
-    if (event.target.nodeName !== "IMG") {
-        return;
-    }
-    const modal = document.createElement("div");
-    const newPic = document.createElement("img");
-    newPic.src = event.target.getAttribute("data-source");
-    modal.append(newPic);
+gallery.addEventListener("click", e => {
+    e.preventDefault();
+    if (e.target.nodeName !== "IMG") return;
 
-    instance = basicLightbox.create(modal);
+    const handleEscapeKey = e => { if (e.key === "Escape") instance.close() };
 
-    function closeHandler() {
-        instance.close();
-        modal.removeEventListener("click", closeHandler);
-    }
+    const instance = basicLightbox.create(
+        `<img src="${e.target.dataset.source}">`,
+        {
+            onShow: () => {
+                document.addEventListener("keydown", handleEscapeKey);
+            },
+            onClose: () => {
+                document.removeEventListener("keydown", handleEscapeKey);
+            },
+        });
+    
     instance.show();
-    modal.addEventListener("click", closeHandler);
+
 });
 
-document.addEventListener("keydown", (event) => {
-    if (event.key === "Escape") {
-        instance.close();
-    }
-});
+// gallery.innerHTML = markup;
 
-console.log(galleryItems);
+// let instance;
+
+// gallery.addEventListener("click", (event) => {
+//     event.preventDefault();
+//     if (event.target.nodeName !== "IMG") {
+//         return;
+//     }
+//     const modal = document.createElement("div");
+//     const newPic = document.createElement("img");
+//     newPic.src = event.target.getAttribute("data-source");
+//     modal.append(newPic);
+
+//     instance = basicLightbox.create(modal);
+
+//     function closeHandler() {
+//         instance.close();
+//         modal.removeEventListener("click", closeHandler);
+//     }
+//     instance.show();
+//     modal.addEventListener("click", closeHandler);
+// });
+
+// document.addEventListener("keydown", (event) => {
+//     if (event.key === "Escape") {
+//         instance.close();
+//     }
+// });
+
+// console.log(galleryItems);
